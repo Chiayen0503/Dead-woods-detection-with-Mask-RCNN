b6513d9244a35d9317bbb867b36fc0188410a5bd
Chiayen0503 <37808137+Chiayen0503@users.noreply.github.com>
2019-11-18 18:21:02 +0000

    Update config.py

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -180,7 +180,7 @@ class Config(object):
     # implementation.
     LEARNING_RATE = 0.001
     LEARNING_MOMENTUM = 0.9
-    OPTIMIZER = "ADAM" # default is SGD
+    OPTIMIZER = "SDG" 
 
     # Weight decay regularization
     WEIGHT_DECAY = 0.0001
